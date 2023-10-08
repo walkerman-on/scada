985785b13d4d66019e11da5da965fd051bf24c3f
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-10-08 18:38:43 +0300

    Adding dark and light theme with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import {Routes, Route, Link} from "react-router-dom"
-import {Suspense} from "react"
-import "./index.scss"
-import AccountPage from "./pages/AccountPage/AccountPage";
-import ObjectPage from "./pages/ObjectPage/ObjectPage";
+import {Suspense, useContext} from "react"
+import "./styles/index.scss"
 import {InfographicsPageAsync} from "./pages/InfographicsPage/InfographicsPage.async";
-import TestPage from "./pages/TestPage/TestPage";
 import { AccountPageAsync } from "./pages/AccountPage/AccountPage.async";
 import { TestPageAsync } from "./pages/TestPage/TestPage.async";
 import { ObjectPageAsync } from "./pages/ObjectPage/ObjectPage.async";
+import { Theme, ThemeContext } from "./context/ThemeContext";
+import { useTheme } from "./hooks/useTheme";
+
+
 
 const App = () => {
-   
+    const {theme, changeTheme} = useTheme()
 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to = {"/object"}>Объект</Link>
             <Link to = {"/infographics"} >Инфоргарфика</Link>
             <Link to = {"/test"} >Тестирование</Link>
             <Link to = {"/account"} >Аккаунт</Link>
+            <button style={{background: "white", padding: "5px"}} onClick={changeTheme}>Theme toggle</button>
             <Suspense fallback = {<div>Loading...</div>}>
                 <Routes>
                     <Route path = {"/object"} element = {<ObjectPageAsync />}/>
